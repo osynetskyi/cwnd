d64f4fefdcaf592768ebbcba3f2fe4e4427c5b4a
osynetskyi <alexander.sinetskiy@gmail.com>
2014-11-13 21:40:46 +0200

    messed

--- cwnd.cc
+++ cwnd.cc
@@ -31,7 +31,7 @@
 #include "ns3/double.h"
 
 // simulation time
-#define SIMTIME 170
+#define SIMTIME 30
 
 // number of different strategies
 #define NUM 3
@@ -42,150 +42,14 @@ NS_LOG_COMPONENT_DEFINE ("CWnd");
 
 // global variable for CWnds
 uint32_t cwnds[NUM];
-//double rtts[NUM];
-
-class MyApp : public Application 
-{
-	public:
-		MyApp ();
-		virtual ~MyApp();
-		
-		void Setup (Ptr<Socket> socket, Address address, uint32_t packetSize, uint32_t nPackets, DataRate dataRate);
-		double getTotalTime();
-
-	private:
-		virtual void StartApplication (void);
-		virtual void StopApplication (void);
-
-		void ScheduleTx (void);
-		void SendPacket (void);
-
-	Ptr<Socket>     m_socket;
-	Address         m_peer;
-	uint32_t        m_packetSize;
-	uint32_t        m_nPackets;
-	DataRate        m_dataRate;
-	EventId         m_sendEvent;
-	bool            m_running;
-	uint32_t        m_packetsSent;
-	double 			m_startTime;
-	double 			m_stopTime;
-};
-
-MyApp::MyApp ()
-	: m_socket (0), 
-	m_peer (), 
-	m_packetSize (0), 
-	m_nPackets (0), 
-	m_dataRate (0), 
-	m_sendEvent (), 
-	m_running (false), 
-	m_packetsSent (0),
-	m_startTime (0),
-	m_stopTime (0)
-{
-}
-
-MyApp::~MyApp()
-{
-	m_socket = 0;
-}
-
-double MyApp::getTotalTime() 
-{
-	return m_stopTime - m_startTime;
-}
-
-void MyApp::Setup (Ptr<Socket> socket, Address address, uint32_t packetSize, 
-					uint32_t nPackets, DataRate dataRate)
-{
-	m_socket = socket;
-	m_peer = address;
-	m_packetSize = packetSize;
-	m_nPackets = nPackets;
-	m_dataRate = dataRate;
-	m_startTime = 0;
-	m_stopTime = 0;
-}
-
-void MyApp::StartApplication (void)
-{
-	m_running = true;
-	m_packetsSent = 0;
-	m_startTime = Simulator::Now ().GetSeconds ();
-	m_socket->Bind ();
-	m_socket->Connect (m_peer);
-	SendPacket ();
-}
-
-void MyApp::StopApplication (void)
-{
-	m_running = false;
-	if (m_stopTime == 0) 
-	{
-		m_stopTime = Simulator::Now ().GetSeconds ();
-	}
-
-	if (m_sendEvent.IsRunning ())
-	{
-		Simulator::Cancel (m_sendEvent);
-	}
-
-	if (m_socket)
-	{
-		m_socket->Close ();
-	}
-}
-
-void MyApp::SendPacket (void)
-{
-	Ptr<Packet> packet = Create<Packet> (m_packetSize);
-	m_socket->Send (packet);
-
-	if (++m_packetsSent < m_nPackets)
-	{
-		ScheduleTx ();
-	} else {
-		m_stopTime = Simulator::Now ().GetSeconds ();
-	}
-}
-
-void MyApp::ScheduleTx (void)
-{
-	if (m_running)
-	{
-		Time tNext (Seconds (m_packetSize * 8 / static_cast<double> (m_dataRate.GetBitRate ())));
-		m_sendEvent = Simulator::Schedule (tNext, &MyApp::SendPacket, this);
-	}
-}
 
 static void CwndChange (uint32_t oldCwnd, uint32_t newCwnd)
 {
 	// GetContext is used to find a NodeId of a node that triggered the event
 	int num = Simulator::GetContext () - 1;
 	NS_LOG_UNCOND ("Sender " << num << " " << Simulator::Now ().GetSeconds () << "\t" << newCwnd);
-	/*if(newCwnd > oldCwnd) {
-		rtts[num-1] = Simulator::Now().GetSeconds() - rtts[num-1];
-		if(rtts[num-1] != 0) {
-			NS_LOG_UNCOND ("Approx. throughput " << num << " " << Simulator::Now ().GetSeconds () << "\t" 
-					<< oldCwnd / rtts[num-1] * 8 / 1024 / 1024);
-		}
-		}*/
 	cwnds[num - 1] = newCwnd;
-	/*TypeId tid = TypeId::LookupByName ("ns3::TcpReno");
-	std::stringstream nodeId;
-	nodeId << Simulator::GetContext ();
-	std::string specificNode = "/NodeList/" + nodeId.str () + "/$ns3::TcpL4Protocol/SocketType";
-	if(Simulator::GetContext () == 2) {
-		Config::Set (specificNode, TypeIdValue (tid));
-		NS_LOG_UNCOND ("SET!");
-	}*/
 }
-
-/*static void RwndChange (uint32_t oldRwnd, uint32_t newRwnd)
-{
-	NS_LOG_UNCOND ("RWND " << Simulator::GetContext () - 1 << " " << oldRwnd << " " << newRwnd);
-}*/
 
 static void RttChange (Time oldRtt, Time newRtt)
 {
@@ -194,11 +58,6 @@ static void RttChange (Time oldRtt, Time newRtt)
 	NS_LOG_UNCOND ("Approx. throughput " << num << " " << Simulator::Now ().GetSeconds () << "\t" 
 					<< cwnds[num-1] / newRtt.GetSeconds() * 8 / 1024 / 1024);
 }
-
-/*static void RxDrop (Ptr<const Packet> p)
-{
-	NS_LOG_UNCOND ("RxDrop at " << Simulator::Now ().GetSeconds ());
-}*/
 
 int 
 main (int argc, char *argv[])
@@ -275,13 +134,13 @@ main (int argc, char *argv[])
 	ApplicationContainer sinkApps[NUM];
 	PacketSinkHelper *packetSinkHelper[NUM];
 
-	TypeId tid = TypeId::LookupByName ("ns3::TcpTahoe");
+	TypeId tid = TypeId::LookupByName ("ns3::TcpNewReno");
 	std::stringstream nodeId[NUM];
 	std::string specificNode[NUM];
 	Ptr<Socket> ns3TcpSocket[NUM];
 
-	Ptr<MyApp> app[NUM];
-	Ptr<PacketSink> sink[NUM];
+	//Ptr<MyApp> app[NUM];
+	//Ptr<PacketSink> sink[NUM];
 
 	for(i = 0; i < NUM; i++)
 	{
@@ -302,15 +161,34 @@ main (int argc, char *argv[])
 		ns3TcpSocket[i]->TraceConnectWithoutContext ("RTT", MakeCallback (&RttChange));
 		ns3TcpSocket[i]->SetAttribute("SegmentSize", UintegerValue(alpha[i]));
 		ns3TcpSocket[i]->SetAttribute("SlowStartThreshold", UintegerValue(beta[i]));
-	
-		/// setup the data-sending apps on sender nodes
+	}
+
+		/*// setup the data-sending apps on sender nodes
 		app[i] = CreateObject<MyApp> ();
 		//app[i]->Setup (ns3TcpSocket[i], sinkAddress[i], 1040, 100000, DataRate ("5Mbps"));
 		app[i]->Setup (ns3TcpSocket[i], sinkAddress[i], 1040, 10000, DataRate ("5Mbps"));
 		csmaNodesSend.Get (i + 1)->AddApplication (app[i]);
 		app[i]->SetStartTime (Seconds (0.));
-		app[i]->SetStopTime (Seconds (SIMTIME));
-	}
+		app[i]->SetStopTime (Seconds (SIMTIME));*/
+	BulkSendHelper source1 ("ns3::TcpSocketFactory", InetSocketAddress (csmaInterfacesSend.GetAddress (1), sinkPort));
+	source1.SetAttribute ("MaxBytes", UintegerValue (10000));
+	ApplicationContainer sourceApps1 = source1.Install (csmaNodesSend.Get (1));
+	sourceApps1.Start (Seconds (0.0));
+	sourceApps1.Stop (Seconds (SIMTIME));
+
+	BulkSendHelper source2 ("ns3::TcpSocketFactory", InetSocketAddress (csmaInterfacesSend.GetAddress (2), sinkPort));
+	source2.SetAttribute ("MaxBytes", UintegerValue (10000));
+	ApplicationContainer sourceApps2 = source2.Install (csmaNodesSend.Get (2));
+	sourceApps2.Start (Seconds (0.0));
+	sourceApps2.Stop (Seconds (SIMTIME));
+
+	BulkSendHelper source3 ("ns3::TcpSocketFactory", InetSocketAddress (csmaInterfacesSend.GetAddress (3), sinkPort));
+	source3.SetAttribute ("MaxBytes", UintegerValue (10000));
+	ApplicationContainer sourceApps3 = source3.Install (csmaNodesSend.Get (3));
+	sourceApps3.Start (Seconds (0.0));
+	sourceApps3.Stop (Seconds (SIMTIME));
+
+	//NS_LOG_UNCOND("pwong " << csmaInterfacesSend.GetAddress (1));
 
 	//csmaDevicesReceive.Get (1)->TraceConnectWithoutContext ("PhyRxDrop", MakeCallback (&RxDrop));
 
@@ -319,7 +197,7 @@ main (int argc, char *argv[])
 	Simulator::Destroy ();
 
 	// finding the goodput and printing it to a file
-	std::ofstream outfile;
+	/*std::ofstream outfile;
   	outfile.open ("goodput.dat");
 	for(i = 0; i < NUM; i++)
 	{
@@ -328,7 +206,7 @@ main (int argc, char *argv[])
 		outfile << "Time running " << i + 1 << ": " << app[i]->getTotalTime () << std::endl;
 		outfile << "Goodput " << i + 1 << ": " << sink[i]->GetTotalRx () / app[i]->getTotalTime () << " B/s" << std::endl;
 	}
-	outfile.close();
+	outfile.close();*/
 
 	return 0;
 }
